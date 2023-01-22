2af7a77ebb3409e1ca7bc2c31f7b833d407f54a9
Burak BOZ <burakbozb1@gmail.com>
2023-01-22 12:13:51 +0300

    Add project files.

---
+++ FisJParser/Models/ReceiptModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FisJParser.Models
+{
+    public class ReceiptModel
+    {
+        public string locale { get; set; }
+        public string description { get; set; }
+        public BoundingPoly boundingPoly { get; set; }
+    }
+}
---
+++ FisJParser/Program.cs
@@ -0,0 +1,11 @@
+﻿using FisJParser.Models;
+using System.Text.Json;
+
+List<ReceiptModel> data = new List<ReceiptModel>();
+using (StreamReader r = new StreamReader(@"C:\Users\burak\source\repos\FisJParser\FisJParser\Statics\response.json"))
+{
+    string json = r.ReadToEnd();
+    data = JsonSerializer.Deserialize<List<ReceiptModel>>(json);
+}
+Console.WriteLine(data[0].description);
+Console.ReadLine();
